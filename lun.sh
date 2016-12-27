25c888a05afe695338062518a24f264b3ea39e6d
AlirezaDehghanShoorab <alireza.dehghan.shoorab@gmail.com>
2016-12-27 13:26:34 +0330

    Add files via upload

---
+++ lun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+COUNTER=0
+while [  $COUNTER -lt 5 ]; do
+kill $(pgrep telegram-cli)
+nohup ./run.sh 
+sleep 1
+#let COUNTER=COUNTER+1 
+done
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+./tg -s bot.lua
+done
+
